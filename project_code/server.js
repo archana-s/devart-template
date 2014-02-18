a351641e048fc9fc3a6fa0cfa959bc02d79e563b
Archana S <archana.june@gmail.com>
2014-02-17 23:11:32 -0800

    First Commit with all files for the kaleidoscope

---
+++ project_code/server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var port = 3474;
+var app = express();
+var path = require('path');
+
+var hbs = require('hbs');
+
+app.set('views', __dirname + '/views');
+app.use(express.static('public'));
+
+app.engine('html', hbs.__express);
+app.set('view engine', 'html');
+
+app.listen(port);
+console.log("Express started on " + port);
+
+app.get("/", function(req, res){
+  res.render('index');
+});
+
